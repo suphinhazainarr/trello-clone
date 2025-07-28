006f518e1257e81d147c0d047575d06c511dfd14
suphinhazainarr <suphinhassainar2004@gmail.com>
2025-07-28 06:07:50 +0530

    feat: refactor App component to conditionally hide Navbar on specific routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,36 @@
 // App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BoardForm from "./pages/BoardForm";
-import './app.css' // <-- Make sure this is here!
-import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import './app.css';
 
-function App() {
+function AppLayout() {
+  const location = useLocation();
+  // Hide Navbar on landing, login, and signup pages
+  const hideNavbar = ["/", "/login", "/signup"].includes(location.pathname);
+
   return (
-<Router>
-      <div className="flex flex-col h-screen">
-        <Navbar /> 
-          <Routes>
-            <Route path="/create-board" element={<BoardForm />} />
-            <Route path="/home/*" element={<HomePage />} />
-            <Route path="/" element={<LandingPage />} />
-          </Routes>
-      </div>
-    </Router>
+    <div className="flex flex-col h-screen">
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/create-board" element={<BoardForm />} />
+        <Route path="/home/*" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <AppLayout />
+    </Router>
+  );
+}
