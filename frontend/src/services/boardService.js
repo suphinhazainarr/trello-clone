220aac7681d3a7d9ead418829e62b8bbe1ac3d53
suphinhazainarr <suphinhassainar2004@gmail.com>
2025-07-29 06:48:54 +0530

    add code in boardService file

---
+++ frontend/src/services/boardService.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api";
+
+// Helper to get token from localStorage
+const getAuthHeader = () => ({
+  headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+});
+
+// Create a board
+export const createBoard = async (title) => {
+  const res = await axios.post(`${API_URL}/boards`, { title }, getAuthHeader());
+  return res.data;
+};
+
+// Create a list
+export const createList = async (boardId, title, position = 0) => {
+  const res = await axios.post(`${API_URL}/lists`, { boardId, title, position }, getAuthHeader());
+  return res.data;
+};
+
+// Create a card
+export const createCard = async (listId, title, description = "", assignedTo = [], labels = [], dueDate = null, position = 0) => {
+  const res = await axios.post(
+    `${API_URL}/cards`,
+    { listId, title, description, assignedTo, labels, dueDate, position },
+    getAuthHeader()
+  );
+  return res.data;
+};
